998421076c21c1af2ad667b4b3f47303b555c1ff
TimeOverflow <timeoverflow@yeah.net>
2022-05-11 23:11:53 +0800

    add annotations

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -17,7 +17,11 @@ class ScaledDotProductAttention(nn.Module):
         attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
 
         if mask is not None:
+            # mask: b x 1 x 1 x L
+            # attn: b x n_heads x L x L
             attn = attn.masked_fill(mask == 0, -1e9)
+
+        # 这种 mask 对于 padding 那些行并没有全部 mask,padding 与 word 之间没有 mask，padding 与 padding 之间 mask
 
         attn = self.dropout(F.softmax(attn, dim=-1))
         output = torch.matmul(attn, v)
